7c024bcc895112a910acabd7bc5c5941b1a32e39
billsun93905 <billsun9305@gmail.com>
2023-10-28 20:38:57 -0700

    first commit

---
+++ src/test/java/com/ethapi/blockInfo/BlockInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ethapi.blockInfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlockInfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
